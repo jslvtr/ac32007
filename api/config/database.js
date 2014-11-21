253388aa773263acd09f9a3cfad86bf53ac0bbce
Yago Carballo Porto <yagocarballo@gmail.com>
2014-11-21 17:17:46 +0000

    fixed db_schema bugs

--- api/config/database.js
+++ api/config/database.js
@@ -4,8 +4,7 @@ var assert = require('assert');
 var client = null;
 
 var clientInfo = {
-    contactPoints: [ '127.0.0.1', 'h2' ],
-    keyspace: 'agile_api'
+    contactPoints: [ '127.0.0.1' ]
 };
 
 function InitClient () {
@@ -13,45 +12,56 @@ function InitClient () {
     return client;
 }
 
-function db_schema_error_handling (err, message) {
+function db_schema_error_handling (err, message, callback) {
     if (err) {
         console.error(err);
     } else {
-        console.log(message);
+        console.log(message); // Comment this line to disable database creation logs
+        callback();
     }
 }
 
 function db_schema (delete_all) {
+    client = (client || InitClient());
+
     if (delete_all) {
-        this.client.execute(
+        client.execute(
             'DROP KEYSPACE IF EXISTS agile_api;',
-            db_schema_error_handling('[Success] Keyspace `agile_api` dropped.')
+            function (err) { db_schema_error_handling(err, '[Success] Keyspace `agile_api` dropped.') }
+
         );
     }
 
     // Creates the KeySpace
-    this.client.execute(
+    client.execute(
         'create keyspace if not exists agile_api  WITH replication = {\'class\':\'SimpleStrategy\', \'replication_factor\':1};',
-        db_schema_error_handling('[Success] Keyspace `agile_api` created.')
-    );
-
-    // Creates `users` table
-    this.client.execute(
-        'CREATE TABLE if not exists agile_api.users (' +
-            'full_name varchar,' +
-            'username varchar,' +
-            'email varchar,' +
-            'password varchar,' +
-            'access_token varchar,' +
-            'PRIMARY KEY (username)' +
-        ');',
-        db_schema_error_handling('[Success] Table `users` created.')
-    );
-
-    // Creates an index for the `users` table
-    this.client.execute(
-        'CREATE INDEX IF NOT EXISTS access_token ON users (access_token);',
-        db_schema_error_handling('[Success] Index `access_token` for table `user` created.')
+        function (err) {
+            db_schema_error_handling(err, '[Success] Keyspace `agile_api` created.', function () {
+                // Creates `users` table
+                client.execute(
+                    'CREATE TABLE if not exists agile_api.users (' +
+                    'full_name varchar,' +
+                    'username varchar,' +
+                    'email varchar,' +
+                    'password varchar,' +
+                    'access_token varchar,' +
+                    'PRIMARY KEY (username)' +
+                    ');',
+                    function (err) {
+                        db_schema_error_handling(err, '[Success] Table `users` created.', function () {
+                            // Creates an index for the `users` table
+                            client.execute(
+                                'CREATE INDEX IF NOT EXISTS access_token ON agile_api.users (access_token);',
+                                function (err) {
+                                    db_schema_error_handling(err, '[Success] Index `access_token` for table `user` created.', function () {
+                                    });
+                                }
+                            );
+                        });
+                    }
+                );
+            });
+        }
     );
 }
 

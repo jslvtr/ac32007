505629f766c9d80cb7a7e095b7bfe1b92edcd3b0
Yago Carballo Porto <yagocarballo@gmail.com>
2014-11-21 16:18:53 +0000

    added a error handling method to the creation schema

--- api/config/database.js
+++ api/config/database.js
@@ -13,19 +13,26 @@ function InitClient () {
     return client;
 }
 
+function db_schema_error_handling (err, message) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(message);
+    }
+}
+
 function db_schema (delete_all) {
     if (delete_all) {
-        var query = 'DROP KEYSPACE IF EXISTS agile_api;';
-        this.client.execute(query, function(err, result) {
-                if (err) {
-                    console.error(err);
-                } else {
-                    console.log('[Success] Keyspace `agile_api` dropped.');
-                }
-            }
+        this.client.execute(
+            'DROP KEYSPACE IF EXISTS agile_api;',
+            db_schema_error_handling('[Success] Keyspace `agile_api` dropped.')
         );
     }
 
+    this.client.execute(
+        'create keyspace if not exists agile_api  WITH replication = {\'class\':\'SimpleStrategy\', \'replication_factor\':1};',
+        db_schema_error_handling('[Success] Keyspace `agile_api` created.')
+    );
 }
 
 module.exports = {

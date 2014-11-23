ae8cb245425d269cb95a48cf13789412f49c1a3c
jslvtr <jslvtr@gmail.com>
2014-11-23 17:53:19 +0000

    Fixed recursive call and instead simple check to add "http://" if necessary.

--- api/components/calls.js
+++ api/components/calls.js
@@ -7,49 +7,48 @@ function getMethod(url, callback) {
 
     request(url, function (error, response, body) {
         var fallback = false; //If the client sends url without http it will add it and set this true so if it fails again it will check https://
-        if (url.indexOf("http") > -1 || url.indexOf("https") > -1) {
-            try {
-                var time = (new Date() - start);
-                if (!error && response.statusCode == 200) {
-                    result.push({
-                        status: response.statusCode,
-                        time: time,
-                        header: response.headers,
-                        body: response.body
-                    })
-                    console.log("got json");
-                    callback(result);
-                }   else  if (response.statusCode == null){
-                    console.log("no status code");
-                }   else {
-                    result.push({
-                        status: response.statusCode,
-                        time: time,
-                        header: response.headers,
-                        body: response.body
-                    })
-                    callback(result);
-                }
-            }   catch(ex)    {
-                /*
-
-                                    TODO  IMPORTANT!!!
-
-                        Please check this method when deployed to heroku
-                        When querying https://localhost it breaks nodejs.
-                        This method stops that query from breaking stuff.
-
-                 */
-                console.log("something went wrong: " + ex);
+        if (url.indexOf("https") == -1 && url.indexOf("http") == -1) {
+            url = url + "http://";
+        }
+        try {
+            var time = (new Date() - start);
+            if (!error && response.statusCode == 200) {
                 result.push({
-                    status: 404,
-                    dev_note: "you might want to check this out....",
-                    time: time
+                    status: response.statusCode,
+                    time: time,
+                    header: response.headers,
+                    body: response.body
+                })
+                console.log("got json");
+                callback(result);
+            }   else  if (response.statusCode == null){
+                console.log("no status code");
+            }   else {
+                result.push({
+                    status: response.statusCode,
+                    time: time,
+                    header: response.headers,
+                    body: response.body
                 })
                 callback(result);
             }
-        }   else  {
-            getMethod("http://" + url, callback);
+        }   catch(ex)    {
+            /*
+
+                                TODO  IMPORTANT!!!
+
+                    Please check this method when deployed to heroku
+                    When querying https://localhost it breaks nodejs.
+                    This method stops that query from breaking stuff.
+
+             */
+            console.log("something went wrong: " + ex);
+            result.push({
+                status: 404,
+                dev_note: "you might want to check this out....",
+                time: time
+            })
+            callback(result);
         }
     });
 

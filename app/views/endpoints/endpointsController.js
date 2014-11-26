4fc3cfa14ec310e9ff33c0aaa444bc60e2820a0c
Yago Carballo Porto <yagocarballo@gmail.com>
2014-11-26 17:22:46 +0000

    added more elements to the endpoints view, and installed the ace angular editor

--- app/views/endpoints/endpointsController.js
+++ app/views/endpoints/endpointsController.js
@@ -2,5 +2,55 @@ angular.module('app.endpointsController', [])
     .controller('endpointsController', function($rootScope, $scope, $http, $timeout, $mdSidenav) {
       $rootScope.$broadcast('showTabs', false);
       $scope.title = "Endpoint";
+
+      $scope.endpoint = {
+          type : 'GET',
+          title : 'Fetch Users',
+          description : 'Euismod Pharetra Risus Tortor.',
+          url : 'https://api.demo.com/users',
+          heads : [
+              {
+                  head : 'Content-Type',
+                  value : 'application/json; charset=utf-8'
+              },
+              {
+                  head : 'Authorization',
+                  value : 'Bearer 87932ee1248c284fa36476bbc82ed9f7'
+              }
+          ],
+          url_params : [],
+          body : ""
+      };
+
+      $scope.project = {
+          title : 'Sample Project',
+          description : 'Sed posuere consectetur est at lobortis. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+          endpoints : [
+              {
+                  type : 'GET',
+                  title : 'Fetch Users',
+                  description : 'Euismod Pharetra Risus Tortor.',
+                  url : 'https://api.demo.com/users'
+              },
+              {
+                  type : 'GET',
+                  title : 'Fetch Users',
+                  description : 'Euismod Pharetra Risus Tortor.',
+                  url : 'https://api.demo.com/users'
+              },
+              {
+                  type : 'GET',
+                  title : 'Fetch Users',
+                  description : 'Euismod Pharetra Risus Tortor.',
+                  url : 'https://api.demo.com/users'
+              },
+              {
+                  type : 'GET',
+                  title : 'Fetch Users',
+                  description : 'Euismod Pharetra Risus Tortor.',
+                  url : 'https://api.demo.com/users'
+              }
+          ]
+      };
     }
 );
